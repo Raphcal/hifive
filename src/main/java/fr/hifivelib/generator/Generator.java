8d15fcd7fde502361fe4515e0ec2a44f438c4d17
Raphaël C. <ddaeke-github@yahoo.fr>
2016-02-11 22:33:21 +0100

    Added license plugin and the license header to source files.

--- src/main/java/fr/hifivelib/generator/Generator.java
+++ src/main/java/fr/hifivelib/generator/Generator.java
@@ -1,5 +1,27 @@
 package fr.hifivelib.generator;
 
+/*
+ * #%L
+ * Hifive
+ * %%
+ * Copyright (C) 2016 Raphcal
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 /**
  *
  * @author Raphaël Calabro (ddaeke-github at yahoo.fr)
