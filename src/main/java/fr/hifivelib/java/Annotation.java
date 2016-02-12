02b528f1b95a61dea00953b2d7187d770d01a32d
Raphaël <ddaeke-github@yahoo.fr>
2016-02-12 15:39:54 +0100

    Added empty classes to prepare the implementation of a Java file parser.

---
+++ src/main/java/fr/hifivelib/java/Annotation.java
@@ -0,0 +1,37 @@
+package fr.hifivelib.java;
+
+/*
+ * #%L
+ * Hifive
+ * %%
+ * Copyright (C) 2016 Raphaël Calabro
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * Represent an annotation.
+ * 
+ * @param <T> Type of the value.
+ * 
+ * @author Raphaël Calabro (ddaeke-github at yahoo.fr)
+ */
+public class Annotation<T> {
+	
+	private String name;
+	private T value;
+	
+}
